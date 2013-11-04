d4b4abd4bd84dcd95b07eabb18606bbf1c3a3971
Eran Keydar <ekeydar@gmail.com>
2013-11-04 13:40:38 +0200

    wip

--- main.py
+++ main.py
@@ -3,17 +3,24 @@ import requests
 
 from math import radians, cos, sin, asin, sqrt
 
+USERS = None
+
 class User(object):
 	def __init__(self,ju):
 		self.name = ju['name']
 		self.address = ju['address']
-		self.coords = self.geocode()
+		self.coords = geocode(self.address)
 
-	def geocode(self):
-		#print self.name
-		params=dict(address=self.address,sensor='false')
-		r =  requests.get('http://maps.googleapis.com/maps/api/geocode/json',params=params)
-		return r.json()['results'][0]['geometry']['location']
+	def get_distance_dict(self,loc):
+		result = self.__dict__
+		result['distance'] = haversine(self.coords,loc)
+		return result
+
+def geocode(address):
+	#print self.name
+	params=dict(address=address,sensor='false')
+	r =  requests.get('http://maps.googleapis.com/maps/api/geocode/json',params=params)
+	return r.json()['results'][0]['geometry']['location']
 
 def haversine(loc1, loc2):
     """
@@ -39,9 +46,17 @@ def build_users(filename):
 		users.append(User(u))
 	return users
 
+def find_distances_from(address):
+	address_loc = geocode(address)
+	result = []
+	for u in USERS:
+		result.append(u.get_distance_dict(address_loc))
+	return result
+
 def main():
-	users = build_users('input.json')
-	for u in users:
+	global USERS
+	USERS = build_users('input.json')
+	for u in USERS:
 		print u.name
 		print u.coords
 
